6f7dc8951101ad7100219c13c3c3e2ac9ffcac42
Pablito471 <pablohernanbrito@gmail.com>
2026-01-19 22:06:51 -0300

    eliminacion de carpeta proyecto

--- proyecto/frontend/src/components/Loading.jsx
+++
@@ -1,8 +0,0 @@
-export const Loading = ({ message = "Cargando..." }) => {
-  return (
-    <div className="flex justify-center items-center py-8">
-      <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600 mr-3"></div>
-      <p className="text-gray-600">{message}</p>
-    </div>
-  );
-};
